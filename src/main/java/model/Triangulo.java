0e1296970c48bb6a85283c0b213eaf0f031375ce
Iudhi <iudhicunhasantos0@gmail.com>
2026-05-08 03:21:50 -0300

    feat(model): criação dos métodos gets, sets, imprimir,  leitura e calculo

--- src/main/java/model/Triangulo.java
+++ src/main/java/model/Triangulo.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author iudhi
- */
+import javax.swing.JOptionPane;
+
 public class Triangulo {
-    
+
+    private int base;
+    private int altura;
+    private double area;
+
+    public Triangulo() {
+        this(0, 0);
+    }
+
+    public Triangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public void leitura() {
+        setBase(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a base: ")));
+        setAltura(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a altura: ")));
+    }
+
+    public double calculo(double area) {
+        area = ((getBase() * getAltura()) / 2);
+        return area;
+    }
+
+    public void imprimir() {
+        JOptionPane.showMessageDialog(null, "A área do triangulo é: " + area);
+    }
 }
