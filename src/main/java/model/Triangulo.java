38885acd0f991ccd1afc1d162459d51d97b7b74d
Iudhi <iudhicunhasantos0@gmail.com>
2026-05-08 03:44:05 -0300

    fix: correção do método calculo e projeto finalizado

--- src/main/java/model/Triangulo.java
+++ src/main/java/model/Triangulo.java
@@ -37,17 +37,21 @@ public class Triangulo {
         this.area = area;
     }
 
+    public double getArea() {
+        return calculo();
+    }
+
     public void leitura() {
         setBase(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a base: ")));
         setAltura(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a altura: ")));
     }
 
-    public double calculo(double area) {
-        area = ((getBase() * getAltura()) / 2);
+    private double calculo() {
+        setArea((getBase() * getAltura()) / 2.0);
         return area;
     }
 
     public void imprimir() {
-        JOptionPane.showMessageDialog(null, "A área do triangulo é: " + area);
+        JOptionPane.showMessageDialog(null, "A área do triangulo é: " + getArea());
     }
 }
--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package principal;
+import model.Triangulo;
 
-/**
- *
- * @author iudhi
- */
 public class Principal {
-    
+      public static void main(String[] args) {
+    Triangulo tri = new Triangulo();
+    tri.leitura();
+    tri.imprimir();
+}
 }
